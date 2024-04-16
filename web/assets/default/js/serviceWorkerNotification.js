ba6b683cebe4a9fd052efdb2f1a2747f78e801c2
Parley <cristi@skyprivate.com>
2024-04-16 16:35:42 +0300

    updatesB

--- web/assets/default/js/serviceWorkerNotification.js
+++ web/assets/default/js/serviceWorkerNotification.js
@@ -1,18 +1,31 @@
 console.log('Service Worker notification Loaded ...');
 
-self.addEventListener('push',async function(e){
+self.addEventListener('push',function(e){
 
     const data = e.data.json();
 
     console.log('Push Recieved ...');
+
+    // const a = {
+    //     body: e.content,
+    //     icon: e.icon,
+    //     image: e.image,
+    //     data: e,
+    //     actions: e.buttons,
+    //     tag: r.tag,
+    //     requireInteraction: r.persistNotification,
+    //     renotify: !0,
+    //     badge: e.badge,
+    //     vibrate: e.vibrate
+    // };
     
-    const sent = await self.registration.showNotification(data.title, {
+    self.registration.showNotification(data.title, {
         body: data.body,
         icon: data.icon,
         image: data.image,
         urgency: 'very-high', // Set priority to high,
-        requireInteraction: true // Set requireInteraction to true
+        requireInteraction: true, // Set requireInteraction to true
+        renotify: !0,
     });
 
-    console.log(sent);
 });
