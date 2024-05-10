57098de1487d2f8ef084b034a36e78e4ecc08d1b
Parley <cristi@skyprivate.com>
2024-05-10 10:35:18 +0300

    updates200524

--- components/home/home.api.js
+++ components/home/home.api.js
@@ -1,12 +1,13 @@
 const Controller = require('./home.controller');
 const View = require('../../middlewares/View.js');
 const Tracer = require('tracer').colorConsole();
-const crypto = require('crypto');
 
 const HomeAPI = {
 	get: {
 		handler: async (request,reply) => {
 			
+			Tracer.info(request.headers['user-agent']);
+
 			return new View(request,reply)
 				.send('home/index.eta',await Controller.getDefault({}));
 		},
