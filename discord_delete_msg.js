2a01ffab2a1b48a281fd4b92927ef9c74e2a2a21
Parley <cristi@skyprivate.com>
2025-08-05 11:27:38 +0300

    addRadio

--- discord_delete_msg.js
+++ discord_delete_msg.js
@@ -1,30 +1,30 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+// const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
-});
+// const client = new Client({
+//     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+// });
 
-const TOKEN = 'MTMzNTk3MDI3ODQ5MjA4MjIwOA.GE0z34.yaDqHPJesVeuc2F3jC7zyRRrag5GhBM0T7b56c';
-const CHANNEL_ID = '1334450462337925150';
+// const TOKEN = 'MTMzNTk3MDI3ODQ5MjA4MjIwOA.GE0z34.yaDqHPJesVeuc2F3jC7zyRRrag5GhBM0T7b56c';
+// const CHANNEL_ID = '1334450462337925150';
 
-client.once('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}`);
+// client.once('ready', async () => {
+//     console.log(`Logged in as ${client.user.tag}`);
 
-    const channel = await client.channels.fetch(CHANNEL_ID);
-    let messages;
+//     const channel = await client.channels.fetch(CHANNEL_ID);
+//     let messages;
 
-    do {
-        messages = await channel.messages.fetch({ limit: 100 }); // Fetch max 100 messages
+//     do {
+//         messages = await channel.messages.fetch({ limit: 100 }); // Fetch max 100 messages
 
-        console.log(messages.size + " messages fetched, deleting...");
-        const result = await channel.bulkDelete(messages);
+//         console.log(messages.size + " messages fetched, deleting...");
+//         const result = await channel.bulkDelete(messages);
         
-    } while (messages.size > 0);
+//     } while (messages.size > 0);
 
-    console.log("All messages deleted!");
-    process.exit(); // Exit script
-});
+//     console.log("All messages deleted!");
+//     process.exit(); // Exit script
+// });
 
-client.login(TOKEN);
+// client.login(TOKEN);
 
-return;
+// return;
